b0c3dd08e7095d386b067f20088e5ab4e431e69e
Zayan Khanzada <152228627+Zayankhanzada@users.noreply.github.com>
2023-12-23 00:18:37 +0500

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -8,7 +8,7 @@ function Type() {
         strings: [
           "Software Developer",
           "Freelancer",
-          "MERN Stack Developer",
+          "React UI Developer",
           "Open Source Contributor",
         ],
         autoStart: true,
